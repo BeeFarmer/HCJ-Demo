e65c1718642c45bdcf9aad1080ea62bdcd90b13a
doug65535 <doug65535@gmail.com>
2021-09-01 00:43:22 -0700

    finished

--- src/index.js
+++ src/index.js
@@ -1,28 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import {BrowserRouter as Router , Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import './index.css';
-import App from './App';
-import ReactAssi from './reactAssi'
+import "./index.css";
+import App from "./App";
+import ReactAssi from "./reactAssi";
 
-
-import { Provider } from 'react-redux'
-import STORE from './store'
+import { Provider } from "react-redux";
+import STORE from "./store";
 
 ReactDOM.render(
-
   <Router>
-      <Provider store={STORE}>
-    <div>
-      <Route exact path="/" component={App} />
-      <Route  path="/reactAssi.js" component={ReactAssi} />
-    </div>
+    <Provider store={STORE}>
+      <div>
+        <Route exact path="/" component={App} />
+        <Route path="/reactAssi.js" component={ReactAssi} />
+      </div>
     </Provider>
   </Router>,
 
- 
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
--- src/reactAssi.js
+++ src/reactAssi.js
@@ -1,87 +1,220 @@
-import React,{useEffect} from 'react'
-import { useHistory } from 'react-router';
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router";
 
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 
+function reduxP(props) {
+  const useHis = useHistory();
+  const linkTO = () => {
+    useHis.push({ pathname: "./" });
+  };
 
- function reduxP(props){
-      const useHis = useHistory()
-      const linkTO = () =>{
-        useHis.push({pathname:'./'});
-      }
+  const pluss = () => {
+    props.plus();
+  };
+  const minuss = () => {
+    props.minus();
+  };
+  const oddPlusss = () => {
+    if (props.coutV % 2 !== 0) props.plus();
+  };
 
-  const pluss = ()=>{
-    props.plus()
-  }
-  const minuss = () =>{
-    props.minus()
-  }
-  const oddPlusss = ()=>{
-      if(props.coutV %2 !== 0) props.plus()
-  }
-
-  const asyInc = ()=>{
-  setTimeout(() => {
-    props.plus()
-  }, 1000);
-  }
-  let inte ;
+  const asyInc = () => {
+    setTimeout(() => {
+      props.plus();
+    }, 1000);
+  };
+  let inte;
   useEffect(() => {
-      if(props.isOn) {
-        inte = setInterval(() => {
-          props.plus()
-        }, 1000);
-      }else{
-        clearInterval(inte)
-      }
-     
+    if (props.isOn) {
+      inte = setInterval(() => {
+        props.plus();
+      }, 1000);
+    } else {
+      clearInterval(inte);
+    }
 
     return () => {
-      clearInterval(inte)
+      clearInterval(inte);
+    };
+  }, [props.isOn]);
+
+  const IntervelInc = (e) => {
+    props.switchS();
+  };
+
+  //// start todolist
+  let input;
+  const addTodoFunc = (e) => {
+    e.preventDefault();
+    if (!input.value.trim()) {
+      return;
     }
-  }, [props.isOn])
+    props.addTodo(input.value);
+    input.value = "";
+  };
 
-  const IntervelInc = ()=>{
-     props.switchS()
-  }
+  const TodoList = () => (
+    <ul>
+      {props.todos.map((todo) => (
+        <li
+          key={todo.id}
+          style={{ textDecoration: todo.completed ? "line-through" : "none" }}
+          onClick={() => props.toggleTodo(todo.id)}
+        >
+          {todo.text}
+        </li>
+      ))}
+    </ul>
+  );
+  const [asc, setAsc] = useState(-1);
 
-    return( 
-        <div>
-        <button style={{display:'block',margin : '20px 10px auto',height:50}} onClick = {linkTO}>GO BACK</button>
-        <div style={{margin : '0px auto',height:50,width:100}}> YOU CLICKED :{props.coutV}</div>
+  useEffect(() => {
+    if (asc) {
+      props.todos.sort((a, b) => {
+        if (a.text < b.text) {
+          return -1;
+        }
+        if (a.text > b.text) {
+          return 1;
+        }
+        return 0;
+      });
+      setAsc();
+    } else {
+      props.todos.sort((a, b) => {
+        if (a.text < b.text) {
+          return 1;
+        }
+        if (a.text > b.text) {
+          return -1;
+        }
+        return 0;
+      });
+      setAsc();
+    }
+  }, [asc]);
 
-        <button style={{display:'inline',marginTop : '50px',height:50,width:100}} onClick={pluss}>+</button>
-        <button style={{display:'inline',height:50,width:100}}  onClick={minuss}>-</button>
-        <button style={{display:'inline',height:50,width:100}}  onClick={oddPlusss}>oddInc</button>
-        <button style={{display:'inline',height:50,width:100}}  onClick={asyInc}>oddInc</button>
-        <button style={{display:'block',height:50,width:100}}  onClick={IntervelInc}>{!props.isOn?'IntervelInc':'Stop'}</button>
-        
+  const [couterVisible, setCouterVisible] = useState(true);
+  const [todoVisible, setTodoVisible] = useState(true);
+
+  return (
+    <div>
+      <button
+        style={{ display: "block", margin: "20px 10px auto", height: 50 }}
+        onClick={linkTO}
+      >
+        GO BACK
+      </button>
+
+      <button
+        style={{ margin: "50px auto", height: 50, width: 100 }}
+        onClick={() => setCouterVisible(!couterVisible)}
+      >
+        {couterVisible ? "CouterChecked" : "checkCouter"}
+      </button>
+      <div style={{ display: couterVisible ? "block" : "none" }}>
+        <div style={{ margin: "0px auto", height: 50, width: 100 }}>
+          {" "}
+          YOU CLICKED :{props.coutV}
         </div>
-    )
+        <button
+          style={{
+            display: "inline",
+            marginTop: "50px",
+            height: 50,
+            width: 100,
+          }}
+          onClick={pluss}
+        >
+          +
+        </button>
+        <button
+          style={{ display: "inline", height: 50, width: 100 }}
+          onClick={minuss}
+        >
+          -
+        </button>
+        <button
+          style={{ display: "inline", height: 50, width: 100 }}
+          onClick={oddPlusss}
+        >
+          oddInc
+        </button>
+        <button
+          style={{ display: "inline", height: 50, width: 100 }}
+          onClick={asyInc}
+        >
+          oddInc
+        </button>
+        <button
+          style={{ display: "block", height: 50, width: 100 }}
+          onClick={IntervelInc}
+        >
+          {!props.isOn ? "IntervelInc" : "Stop"}
+        </button>
+      </div>
+
+      <button
+        style={{ margin: "50px auto", height: 50, width: 100 }}
+        onClick={() => setTodoVisible(!todoVisible)}
+
+      >
+        {todoVisible ? "TODOChecked" : "checkTODO"}
+      </button>
+      <div style={{ display: todoVisible ? "block" : "none" }}>
+        <div style={{ marginTop: 50 }}>
+          {/* //callback  */}
+          <input ref={(node) => (input = node)} />
+          <button onClick={addTodoFunc}>Add Todo</button>
+        </div>
+        <TodoList />
+        <button onClick={() => setAsc(1)}>A-Z</button>
+        <button onClick={() => setAsc(0)}>Z-A</button>
+      </div>
+    </div>
+  );
 }
-// Object of action creators ****useFunc
+// Object of action creators
 
- const plus = ()=>({
-  type:'INCREMENT'
-})
- const minus = ()=>({
-  type:'DECREMENT'
- })
- const switchS = ()=>({
-  type:'SWITCH'
-})
+const plus = () => ({
+  type: "INCREMENT",
+});
+const minus = () => ({
+  type: "DECREMENT",
+});
+const switchS = () => ({
+  type: "SWITCH",
+});
 
- const mapDispatchToProps = {
+let nextTodoId = 0;
+const addTodo = (text) => ({
+  type: "ADD_TODO",
+  id: nextTodoId++,
+  text,
+});
+
+const toggleTodo = (id) => ({
+  type: "TOGGLE_TODO",
+  id,
+});
+
+//****useFunc
+const mapDispatchToProps = {
   plus,
   minus,
-  switchS
-}
- //invoke when update ****useValue
- const mapStateToProps = function(state) {
+  switchS,
+  addTodo,
+  toggleTodo,
+};
+
+//invoke when update ****useValue
+const mapStateToProps = function (state) {
   return {
     isOn: state.switchValue,
-    coutV:state.couterValue
-  }
-}
+    coutV: state.couterValue,
+    todos: state.todos,
+  };
+};
 
-export default connect(mapStateToProps,mapDispatchToProps)(reduxP)
+export default connect(mapStateToProps, mapDispatchToProps)(reduxP);
